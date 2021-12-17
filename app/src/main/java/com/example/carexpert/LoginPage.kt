c3dd2be598bddb11fcffb654c88c84ea41019586
bryanliauw <94959155+bryanliauw@users.noreply.github.com>
2021-12-18 01:02:10 +0700

    vert

---
+++ app/src/main/java/com/example/carexpert/LoginPage.kt
@@ -0,0 +1,68 @@
+package com.example.carexpert
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
+import org.w3c.dom.Text
+
+class LoginPage : AppCompatActivity() {
+    var dataNama : ArrayList<daftarUser> = ArrayList<daftarUser>()
+
+    lateinit var _username : EditText
+    lateinit var _password : EditText
+    lateinit var _FailedSuccess : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+
+        val _daftar = findViewById<TextView>(R.id.daftar)
+        _daftar.setOnClickListener{
+            val eIntent = Intent(this@LoginPage, SignUpPage::class.java)
+            startActivity(eIntent)
+        }
+
+        val db : FirebaseFirestore = FirebaseFirestore.getInstance()
+
+        _username = findViewById<EditText>(R.id.username)
+        _password = findViewById<EditText>(R.id.password)
+        _FailedSuccess = findViewById<TextView>(R.id.FailedSucess)
+
+        val _verifikasi = findViewById<Button>(R.id.verifikasi)
+        _verifikasi.setOnClickListener{
+            readData(db, _username.text.toString(), _password.text.toString())
+        }
+    }
+
+    fun readData(db: FirebaseFirestore, username : String, password: String){
+        db.collection("tbUser")
+            .get()
+            .addOnSuccessListener { result ->
+                dataNama.clear()
+                var found = 0
+                for (document in result){
+                    if (document.data.get("username").toString() == username &&
+                        document.data.get("password").toString() == password){
+                        found = 1
+                        _FailedSuccess.setText("Success")
+                        _username.setText("")
+                        _password.setText("")
+                        //Pindah Intent
+
+                    }
+                }
+                if (found == 0){
+                    _FailedSuccess.setText("Username/Password tidak sesuai")
+                }
+            }
+            .addOnFailureListener{
+                Log.d("Firebase", it.message.toString())
+            }
+    }
+}
---
+++ app/src/main/java/com/example/carexpert/daftarUser.kt
@@ -0,0 +1,8 @@
+package com.example.carexpert
+
+data class daftarUser(
+    var nama : String,
+    var username : String,
+    var email : String,
+    var password : String
+)
